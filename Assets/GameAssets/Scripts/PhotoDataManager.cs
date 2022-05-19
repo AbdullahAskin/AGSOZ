1935f89c0f26bf6e75afb97b3776dff8ce2b606a
Abdullah Ahmet Askin <52455731+AbdullahAskin@users.noreply.github.com>
2022-05-19 13:04:38 +0300

    Unsplash api imported

---
+++ Assets/GameAssets/Scripts/PhotoDataManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PhotoDataManager : MonoBehaviour
+{
+    [SerializeField] private string clientId;
+    [SerializeField] private int perPage;
+    [SerializeField] private List<RawImage> rawImages;
+
+    private void Start()
+    {
+        StartCoroutine(GetDictionaryData("car"));
+    }
+
+    public IEnumerator GetDictionaryData(string word)
+    {
+        var currentApiUrl = "https://api.unsplash.com/search/photos?query=" + word + "&client_id=" + clientId + "&per_page="+perPage;
+        var webRequest = UnityWebRequest.Get(currentApiUrl);
+        yield return webRequest.SendWebRequest();
+
+        var pattern = "(?<=thumb.:.)[^\"]*(fit=max)[^\"]+";
+        
+        var rg = new Regex(pattern);
+        var matchedAuthors = rg.Matches(webRequest.downloadHandler.text);
+
+        for (var index = 0; index < matchedAuthors.Count; index++)
+        {
+            if (index + 1 >= rawImages.Count) break;
+            var rawUrl = matchedAuthors[index].Value;
+            var url = rawUrl.Insert(rawUrl.Length - 5, "&");
+            var www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            rawImages[index].texture = myTexture;
+            rawImages[index].SetNativeSize();
+        }
+    }
+}
