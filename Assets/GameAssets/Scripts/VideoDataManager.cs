fae9bc9a2738e3e1e7d08e7b771dff30bd44aebe
Abdullah Ahmet Askin <52455731+AbdullahAskin@users.noreply.github.com>
2022-05-21 01:49:41 +0300

    Video data manager completed

--- Assets/GameAssets/Scripts/VideoDataManager.cs
+++ Assets/GameAssets/Scripts/VideoDataManager.cs
@@ -1,22 +1,45 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Video;
 
 public class VideoDataManager : MonoBehaviour
 {
     [SerializeField] private string clientId;
     [SerializeField] private int perPage;
-    
-    public IEnumerator GetPhotoDatas(string word)
+    [SerializeField] private List<VideoPlayer> videoPlayers;
+
+    public IEnumerator GetVideoDatas(string word)
     {
-        var currentApiUrl = "https://api.unsplash.com/search/photos?query=" + word + "&client_id=" + clientId + "&per_page="+perPage;
+        var currentApiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + clientId + "&limit=" + perPage + "&q=" +
+                            word;
         var webRequest = UnityWebRequest.Get(currentApiUrl);
         yield return webRequest.SendWebRequest();
+        print(currentApiUrl);
 
-        var pattern = "(?<=thumb.:.)[^\"]*(fit=max)[^\"]+";
-        var rg = new Regex(pattern);
-        var videoUrl = rg.Match(webRequest.downloadHandler.text);
-        print(webRequest.downloadHandler.text);
+        var originalPattern = "\"original\"[^}]*";
+        var originalRg = new Regex(originalPattern);
+        var matchedAuthors = originalRg.Matches(webRequest.downloadHandler.text);
+
+        for (var index = 0; index < matchedAuthors.Count; index++)
+        {
+            print(index);
+            if (index == videoPlayers.Count) break;
+            var videoPattern = "(?<=mp4.:.)[^\"]*";
+            var videoRg = new Regex(videoPattern);
+            var videoUrl = videoRg.Match(matchedAuthors[index].Value).Value;
+            LoadVideo(videoPlayers[index], videoUrl);
+        }
+    }
+
+    private void LoadVideo(VideoPlayer videoPlayer, string videoUrl)
+    {
+        videoPlayer.url = videoUrl;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.Prepare();
     }
 }
