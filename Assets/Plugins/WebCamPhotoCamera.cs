3985b20919b3182205ef6f0c6cfb9002c58f46d7
Abdullah Ahmet Askin <52455731+AbdullahAskin@users.noreply.github.com>
2022-05-23 16:42:36 +0300

    Modified web cam photo camera

--- Assets/Plugins/WebCamPhotoCamera.cs
+++ Assets/Plugins/WebCamPhotoCamera.cs
@@ -24,7 +24,6 @@ public class WebCamPhotoCamera : MonoBehaviour
 
         if (!backCam.isPlaying)
             backCam.Play();
-
     }
 
     public void RecordPhoto()
@@ -32,25 +31,16 @@ public class WebCamPhotoCamera : MonoBehaviour
         StartCoroutine(TakePhoto());
     }
 
-    IEnumerator TakePhoto()  // Start this Coroutine on some button click
+    IEnumerator TakePhoto()  
     {
-
-        // NOTE - you almost certainly have to do this here:
-
         yield return new WaitForEndOfFrame();
-
-        // it's a rare case where the Unity doco is pretty clear,
-        // http://docs.unity3d.com/ScriptReference/WaitForEndOfFrame.html
-        // be sure to scroll down to the SECOND long example on that doco page 
 
         Texture2D photo = new Texture2D(backCam.width, backCam.height);
         photo.SetPixels(backCam.GetPixels());
         photo.Apply();
 
-        //Encode to a PNG
         byte[] bytes = photo.EncodeToJPG();
-        //Write out the PNG. Of course you have to substitute your_path for something sensible
-        System.IO.File.WriteAllBytes(@"C:\Users\Admin\Documents\GitHub\AGSOZ\Assets\StreamingAssets\dnn\" + "004545.jpg", bytes);
+        File.WriteAllBytes(Application.dataPath+@"/StreamingAssets/dnn/004545.jpg", bytes);
         rawImage.texture = photo;
     }
 }
